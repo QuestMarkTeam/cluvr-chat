8a3e7154bec9b3bbc31ba2fd7b1c103edc0f7279
sun <hsunvirus@gmail.com>
2025-07-08 05:20:57 +0900

    Build: Build Test

--- src/main/java/com/example/chat/service/GetInfoFromExternalImpl.java
+++ src/main/java/com/example/chat/service/GetInfoFromExternalImpl.java
@@ -43,6 +43,8 @@ public class GetInfoFromExternalImpl implements GetInfoFromExternal {
 		}
 		String sub = jwt.getSubject();
 		Long userId = getUserIdFromSub(sub);
+		log.info("subsubsubsub: {}", sub);
+		log.info("userIduserId: {}", userId);
 		if (userId == null) {
 			throw new BusinessException(ResponseCode.AUTHENTICATION_FAILED, "유저 정보를 찾을 수 없습니다.");
 		}
